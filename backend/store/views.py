29d022dd61d67a705d8efe3bb59fb7d0fd35a4c8
Mohit Kumar <mks.djcet@gmail.com>
2025-11-09 20:42:50 +0530

    first commit

---
+++ backend/store/views.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+
+@api_view(['GET'])
+def get_products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_categories(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
