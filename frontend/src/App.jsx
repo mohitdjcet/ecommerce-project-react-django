fbffe3260c97b1493f0763650e6bd3dd62a1ac79
Mohit Kumar <mks.djcet@gmail.com>
2025-11-10 17:15:34 +0530

    Updated code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
-import ProductList from "../pages/ProductList";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import ProductList from "./pages/ProductList";
+import ProuctDetails from "./pages/ProductDetails";
 
 function App() {
     return (
-        <div>
-            <ProductList />
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<ProductList/>}/>
+                <Route path="/product/:id" element={<ProuctDetails/>}/>
+            </Routes>
+        </Router>
     );
 }
 
