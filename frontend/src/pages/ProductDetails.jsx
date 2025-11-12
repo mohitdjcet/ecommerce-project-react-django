8230709dc0bbd2996cec2c9accd17189633739bf
Mohit Kumar <mks.djcet@gmail.com>
2025-11-12 22:05:25 +0530

    Updated Day-7 code

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -55,7 +55,7 @@ function ProductDetails() {
             <p className="text-2xl font-semibold text-green-600 mb-6">
               {product.price}
             </p>
-            <button onClick={()=> addToCart(product)} className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition">
+            <button onClick={()=> addToCart(product.id)} className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition">
                 Add to Cart 🛒
             </button>
             {/* Home Button */}
